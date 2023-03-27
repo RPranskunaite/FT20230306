8a9be328f8e0cda54987152667756ad97765368e
Rugilė Pranskūnaitė <ru159gile@gmail.com>
2023-03-27 11:20:16 +0300

    GetCharsCounter

--- app/src/main/java/com/example/ft20230306/utils/TextCounter.java
+++ app/src/main/java/com/example/ft20230306/utils/TextCounter.java
@@ -1,6 +1,11 @@
 package com.example.ft20230306.utils;
 
 public class TextCounter {
-    private static String getCharsCount(String inputSting){
+    public static String getCharsCount(String inputSting) {
         return String.valueOf(inputSting.length());
+    }
+
+    public static int countWords(String toString) {
+        return 0;
+    }
 }
